e1ad6f0efdb9ab6a6a70bbb1e59a5bbe217864ba
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-01-17 07:28:36 +0545

    added login page content

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,6 +1,10 @@
+import {useState} from 'react'
 const Login = ()=>{
+    const [color ,setColor]= useState('black')
     return (
-        <h1>login page</h1>
+        <h1 style={{color: color}}
+            onMouseOver={()=> console.log("i am over tthe text")}
+        >login page</h1>
     )
 }
 export default Login
