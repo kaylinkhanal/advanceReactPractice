4e691215889149dd7e5edd5550ad0dfcd7a20808
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-01-18 08:03:22 +0545

    added handing logic

--- src/containers/playground.js
+++ src/containers/playground.js
@@ -1,18 +1,40 @@
 import {useState} from 'react'
 const PlayGround = ()=>{
     const winningName = 'rhinocerous'
+    const splittedWinningNameArr = winningName.split('')
+    //splittedWinningNameArr will be [ 'r', 'h', 'i', 'n', 'o', 'c', 'e', 'r', 'o', 'u', 's' ]
     const [inputCharacters, setInputCharacters] =  useState('')
-    const [allEnteredChar, setAllEnteredChar] = useState(['a','c'])
-
+    const [allEnteredChar, setAllEnteredChar] = useState([])
+    const [wrongCount, setWrongCount] = useState(0)
     const triggerSubmit = ()=> {
-        // 
+        const bckUpList = [...allEnteredChar]
+        const firstLetter = inputCharacters[0]
+        if (splittedWinningNameArr.includes(firstLetter)){
+          
+        }else{
+            setWrongCount(wrongCount+1)
+        }
+        bckUpList.push(inputCharacters[0])
+        setAllEnteredChar(bckUpList)
     }
+
+    const hangingImageArr = ['O', ['/','|', '\\'], '|', ['/', '\\']]
     return (
         <>
       Hangman game
       <input placeholder="enter name" onKeyUp={(e)=> setInputCharacters(e.target.value)}/>
       <button onClick={()=> triggerSubmit()}>Submit</button>
        {allEnteredChar}
+     
+        {splittedWinningNameArr.map(item=>{
+            return <li>{item}</li>
+        })}
+        <br/>
+        {wrongCount}
+
+        {hangingImageArr.map((item)=>{
+            return(<h1>{item}</h1>)
+        })}
         </>
     )
 }
